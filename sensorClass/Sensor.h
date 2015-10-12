ea266bfd8c4640f8b00f0d5fbef676ad72ae1935
BenStringer3 <ben.c.stringer@gmail.com>
2015-10-12 18:41:42 -0400

    Sensor

---
+++ sensorClass/Sensor.h
@@ -0,0 +1,28 @@
+
+#ifndef Sensor_h
+#define Sensor_h
+
+#include "Arduino.h"
+
+enum Type{
+  WALL,
+  DISTANCE,
+  GENERIC
+};
+
+class Sensor
+{
+  public:
+    Sensor(Type whatKind, int pin);
+    Sensor(Type whatKind, int pin, int pin2);
+    int reed(Type whatKind);
+    float distance();
+    //boolean wall();
+  private:
+    int _pin;
+    int _pin2;
+    Type _whatKind;
+    
+};
+
+#endif
