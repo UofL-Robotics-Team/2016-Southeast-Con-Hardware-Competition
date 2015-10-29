ce3583c5094891840c1e83b93e07850b05ddd2b9
BenStringer3 <ben.c.stringer@gmail.com>
2015-10-28 20:01:17 -0400

    sensor class
    with q test

--- Arduino/sensorClass/Sensor.cpp
+++ Arduino/sensorClass/Sensor.cpp
@@ -2,6 +2,7 @@
 #include "Arduino.h"
 #include "Sensor.h"
 
+
 Sensor::Sensor(Type whatKind, int pin) {
   pinMode(pin, INPUT);
   _whatKind = whatKind;
@@ -40,14 +41,68 @@ int Sensor::reed(Type _whatKind) {
   }
 }
   float Sensor::floatyRadar(){
-    digitalWrite(_pin, LOW);
-      delayMicroseconds(2);
-      digitalWrite(_pin, HIGH);
-      delayMicroseconds(10);
-      digitalWrite(_pin, LOW);
-      //microsec / 70 / 2;
-       //unsigned long derp = pulseIn(_pin2, HIGH);// / 70 / 2;
-      //return derp; //pulseIn(_pin2, HIGH);// / 70 / 2;
-      return 0.0069 * pulseIn(_pin2, HIGH) + 0.4921;
+    noInterrupts();
+	const int _numReadings = 3;
+	float qVal = 1;
+	float readings[_numReadings];
+	float sum = 0;
+	float range;
+	float qValueLow;
+	float qValueHigh;
+
+	//Table of Q values
+	switch (_numReadings) {
+	case 3: qVal = 0.99; break;
+	case 4: qVal = 0.93; break;
+	case 5: qVal = 0.82; break;
+	case 6: qVal = 0.74; break;
+	case 7: qVal = 0.68; break;
+	case 8: qVal = 0.63; break;
+	}
+
+	//populate array with sonar readings
+	for (int i = 0; i < _numReadings; i++) {
+		digitalWrite(_pin, LOW);
+		delayMicroseconds(2);
+		digitalWrite(_pin, HIGH);
+		delayMicroseconds(10);
+		digitalWrite(_pin, LOW);
+		readings[i] = 0.0069 * pulseIn(_pin2, HIGH) + 0.4921;
+		sum = sum + readings[i];
+	}
+	//sort array from least to greatest
+	for (int i = 0; i < (_numReadings - 1); i++) {
+		for (int o = 0; o < (_numReadings - (i + 1)); o++) {
+			if (readings[o] > readings[o + 1]) {
+				float t = readings[o];
+				readings[o] = readings[o + 1];
+				readings[o + 1] = t;
+			}
+		}
+	}
+	//determine Q values for low and high ends of range
+	range = abs(readings[0] - readings[(_numReadings - 1)]);
+	qValueLow = abs((readings[0] - readings[1]) / range);
+	qValueHigh = abs((readings[(_numReadings-1)] - readings[_numReadings-2]) / range);
+	//if the low end value deviates, don't include it in sum
+	if (qValueLow > qVal) {
+		sum = 0;
+		for (int i = 1; i <= _numReadings; i++) {
+			sum = sum + readings[i];
+		}
+		return sum / (_numReadings - 1);
+	}
+	//if the high end deviates,don't include it in sum
+	else if (qValueHigh > qVal) {
+		sum = 0;
+		for (int i = 0; i < _numReadings; i++) {
+			sum = sum + readings[i];
+		}
+		return sum / (_numReadings - 1);
+	}
+	//if neither end deviates, return the avg like normal
+	else { return sum / _numReadings; }
+      interrupts();
   }
+  
 
--- Arduino/sensorClass/Sensor.h
+++ Arduino/sensorClass/Sensor.h
@@ -4,11 +4,22 @@
 
 #include "Arduino.h"
 
+//#include "Wire.h"
+////#include <Adafruit_Sensor.h>
+//#include "Adafruit_BNO055.h"
+////#include <utility/imumaths.h>
+
+
 enum Type{
   WALL,
   DISTANCE,
   GENERIC
 };
+
+//    //int poopy = 5;
+//    int _beginStopWatch;
+//    int _stopStopWatch;
+//    int _time;
 
 class Sensor
 {
@@ -18,6 +29,7 @@ class Sensor
     int reed(Type whatKind);
     float floatyRadar();
   private:
+	//int _numReadings = 3;
     int _pin;
     int _pin2;
     Type _whatKind;
